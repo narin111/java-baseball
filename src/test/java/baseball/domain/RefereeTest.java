8affdee15fb36ccfdba94fffa6258a4cec2685d0
narin111 <jungnr11@naver.com>
2022-11-13 01:09:41 +0900

    test(baseball)

--- src/test/java/baseball/domain/RefereeTest.java
+++ src/test/java/baseball/domain/RefereeTest.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public class RefereeTest {
 
     @Test
-    void 스트라이크3(){
+    void 스트라이크3(){ //
         Referee referee = new Referee();
         String result = referee.compare(Arrays.asList(1,2,3), Arrays.asList(1,2,3));
         // assertThat(result).isEqualTo("0볼 2스트라이크");
