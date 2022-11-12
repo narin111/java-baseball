73d4af3ff76b1f61210e85a64b2417f603319dde
narin111 <jungnr11@naver.com>
2022-11-13 00:03:22 +0900

    feat(baseball): 서로다른 임의의 수 3개 생성하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,10 @@
 package baseball;
 
 import baseball.domain.Calculator;
+import baseball.domain.NumberGenerator;
+
+import java.util.List;
+
 /** 객체지향 프로그래밍
   1. 기능을 가지고 있는 클래스를 인스턴스화(=객체)한다.
   2. 필요한 기능을 인스턴스가 수행하게 한다.
@@ -10,6 +14,8 @@ public class Application {
         // main 치면 템플릿 완성됨
         // Calculator calculator =  new Calculator(); // calculator의 instance가 담김
         // System.out.println(calculator.add(1, 3));
-        //
+        final NumberGenerator generator = new NumberGenerator();
+        List<Integer> numbers = generator.createRandomNumbers();
+        System.out.println(numbers);
     }
 }
--- src/main/java/baseball/domain/NumberGenerator.java
+++ src/main/java/baseball/domain/NumberGenerator.java
@@ -1,10 +1,24 @@
 package baseball.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class NumberGenerator {
     public List<Integer> createRandomNumbers(){
-        int[] numbers = {};
-        return null;
+        List<Integer> numbers = new ArrayList<>();
+        // 3개 숫자 담을 때까지
+        // 만약 이미 존재하는 숫자라면 담지 않는다.
+        // 만약 존재하지 않는 숫자라면 담는다.
+        while(numbers.size() < 3){
+            int number = new Random().nextInt(9 )+1;
+            if(numbers.contains(number)){
+                continue;
+            } else{
+                numbers.add(number);
+            }
+        }
+
+        return numbers;
     }
 }
