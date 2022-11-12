5567164d245f526dc6749f1b1b9ddbf4611a7a7b
narin111 <jungnr11@naver.com>
2022-11-13 00:28:49 +0900

    feat(baseball): 몇개의 숫자가 같은지 알 수 있는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,21 +1,30 @@
 package baseball;
 
 import baseball.domain.Calculator;
+import baseball.domain.Judgment;
 import baseball.domain.NumberGenerator;
 
+import java.util.Arrays;
 import java.util.List;
 
-/** 객체지향 프로그래밍
-  1. 기능을 가지고 있는 클래스를 인스턴스화(=객체)한다.
-  2. 필요한 기능을 인스턴스가 수행하게 한다.
-  3. 각 결과를 종합한다. */
+/**
+ * 객체지향 프로그래밍
+ * 1. 기능을 가지고 있는 클래스를 인스턴스화(=객체)한다.
+ * 2. 필요한 기능을 인스턴스가 수행하게 한다.
+ * 3. 각 결과를 종합한다.
+ */
 public class Application {
     public static void main(String[] args) {
         // main 치면 템플릿 완성됨
         // Calculator calculator =  new Calculator(); // calculator의 instance가 담김
         // System.out.println(calculator.add(1, 3));
-        final NumberGenerator generator = new NumberGenerator();
-        List<Integer> numbers = generator.createRandomNumbers();
-        System.out.println(numbers);
+
+//        final NumberGenerator generator = new NumberGenerator();
+//        List<Integer> numbers = generator.createRandomNumbers();
+//        System.out.println(numbers);
+
+        Judgment judgment = new Judgment();
+        final int count = judgment.correctCount(Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 3));
+        System.out.println(count);
     }
 }
--- src/main/java/baseball/domain/Judgment.java
+++ src/main/java/baseball/domain/Judgment.java
@@ -4,7 +4,14 @@ import java.util.List;
 
 public class Judgment {
     public int correctCount(List<Integer> computer, List<Integer> player){
-        return 0;
+        int result = 0;
+        for(int i=0; i<computer.size(); i++){
+            int playerNumber = computer.get(i);
+            if(computer.contains(playerNumber)){
+                result +=1;
+            }
+        }
+        return result;
     }
     public boolean hasPlace(int place, int number){
         return false;
